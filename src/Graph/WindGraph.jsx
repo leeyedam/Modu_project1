466e76decfed4f47c4d5e67db8c1a55177c31e95
leeyedam <93389233+leeyedam@users.noreply.github.com>
2022-07-14 21:25:11 +0900

    Update WindGraph.jsx

--- src/Graph/WindGraph.jsx
+++ src/Graph/WindGraph.jsx
@@ -108,7 +108,6 @@ function BarGraph({ num }) {
                       valueLabelDisplay="auto"
                       color="primary"
                       aria-label="Disabled slider"
-disabled 
                     />
                     <Typography variant="h4" noWrap>
                       {new Date(dt * 1000).getHours() >= 10 ? (
